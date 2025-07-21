66781581a60101eb3629693883b3b8ff0b36a823
Taha Ferhan <132760925+TFerhan@users.noreply.github.com>
2025-07-21 18:49:57 +0100

    Delete KafkaComponent directory

--- KafkaComponent/src/main/java/com/kafmongo/kafmongo/api/GetTickerSymbol.java
+++
@@ -1,5 +0,0 @@
-package com.kafmongo.kafmongo.api;
-
-public class GetTickerSymbol {
-
-}
--- KafkaComponent/src/main/java/com/kafmongo/kafmongo/utils/MetricByTicker.java
+++
@@ -1,6 +0,0 @@
-package com.kafmongo.kafmongo.utils;
-
-public interface MetricByTicker {
-	Double getMetric();
-	String getTicker();
-}
